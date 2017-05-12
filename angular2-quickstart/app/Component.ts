173449fc800a76e8bf54c0329422e11c0414220d
ramsingh <ramsingh_83@yahoo.com>
2017-05-12 17:35:47 +0530

    Removed directory

--- angular2-quickstart/app/Component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'student-app',
-  template: '<student-form></student-form>'
-})
-export class AppComponent { }
--- angular2-quickstart/app/student.ts
+++
@@ -1,8 +0,0 @@
-export class Student {
-  constructor(
-    public rollNo: number,
-    public name: string,
-    public course: string,
-    public branch?: string
-  ) {  }
-}
